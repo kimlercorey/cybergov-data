b92df884e0176db0dd8967fd46566b76c5bbac00
Scott McCaughey <scott_j_mccaughey@omb.eop.gov>
2016-11-22 11:48:27 -0500

    Adding custom template/controller functionality

--- v1/makeexport.js
+++ v1/makeexport.js
@@ -1,66 +1,70 @@
 const exec = require('child_process').exec;
 
-var exportsCount = 4;
 var itemsCount = 0;
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 
-exec('rm *.json; rm -rf articles collections sections;', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  //console.log(`stderr: ${stderr}`);
-  exportsCount = exportsCount -1;
-  processRecords();
-});
+var apiUrl = 'http://localhost/api';
 
-exec('mongoexport --db test-keystone --collection articles --out articles.json --jsonArray', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  //console.log(`stderr: ${stderr}`);
-  exportsCount = exportsCount -1;
-  processRecords();  
-});
+var contentTypes = [
+  'globals',
+  'collections',
+  'sections',
+  'articles',
+  'questions'
+]
+var numberOfContentTypes = contentTypes.length;
+var exportsCount = numberOfContentTypes + 1;
 
-exec('mongoexport --db test-keystone --collection collections --out collections.json --jsonArray', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  //console.log(`stderr: ${stderr}`);
-  exportsCount = exportsCount -1;
-  processRecords();
-});
 
-exec('mongoexport --db test-keystone --collection sections --out sections.json --jsonArray', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  //console.log(`stderr: ${stderr}`);
-  exportsCount = exportsCount -1;
-  processRecords();
-});
+for (var i = 0; i < numberOfContentTypes; i++) {
+  deleteContentType(contentTypes[i]);
+  exportContentType(contentTypes[i]);
+}
+
+
+function deleteContentType(itemType) {
+  exec('rm *.json; rm -rf ' + itemType + ';', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    //console.log(`stderr: ${stderr}`);
+    exportsCount = exportsCount -1;
+    processRecords();
+  });
+}
+
+
+function exportContentType(itemType) {
+
+  exec('mongoexport --db test-keystone --collection ' + itemType + ' --out ' + itemType + '.json --jsonArray', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    //console.log(`stderr: ${stderr}`);
+    exportsCount = exportsCount -1;
+    processRecords();
+  });
+
+}
+
 
 function processRecords(){
-	if (exportsCount == 0) {
-		console.log("Done exporting");
-		startRead("articles");
-		startRead("collections");
-                startRead("sections");
-	} 
+  if (exportsCount == 0) {
+    console.log("Done exporting");
+    for (var i = 0; i < numberOfContentTypes; i++) {
+      startRead(contentTypes[i]);
+    }
+  }
 }
 
 
 function startRead(itemType){
-   
+
    mkdirp( itemType, function(err) {
        if (err) console.log(err);
    });
@@ -73,75 +77,77 @@ function startRead(itemType){
    itemsCount = itemsCount + jsonContent.length;
 
    for (var i = 0; i < jsonContent.length; i++) {
-	console.log("--------------------------------------------"+itemType+"["+i+"]");
-		
-		var item = jsonContent[i]
-		var itemId = item._id.$oid
-		var pre, post
+  console.log("--------------------------------------------"+itemType+"["+i+"]");
 
-		// TRANSFORMATIONS TO COMPLY WITH REGULAR API FORMAT
-		// export feeds transforms id to id {oid:xxxxx}, this converts it back when in this format 
-		if (item._id.$oid != null) item._id = item._id.$oid;
-		// individual items do not include the collection type but angular expects it
-		if (itemType) {
-			pre = '{"'+itemType.slice(0, -1)+'" :';
-			post = '}';
-		}		
+    var item = jsonContent[i]
+    var itemId = item._id.$oid
+    var pre, post
 
-		item = pre + JSON.stringify(item)+ post
+    // TRANSFORMATIONS TO COMPLY WITH REGULAR API FORMAT
+    // export feeds transforms id to id {oid:xxxxx}, this converts it back when in this format
+    if (item._id.$oid != null) item._id = item._id.$oid;
+    // individual items do not include the collection type but angular expects it
+    if (itemType) {
+      pre = '{"'+itemType.slice(0, -1)+'" :';
+      post = '}';
+    }
+
+    item = pre + JSON.stringify(item)+ post
 
              //console.log(itemId);
              //console.dir(item);
 
-	     fs.writeFile(itemType+"/"+itemId+".json", item, function(err) {
+       fs.writeFile(itemType+"/"+itemId+".json", item, function(err) {
                  if(err) {
                        return console.log(err);
                  }
-		 //console.log( "wrote:", item); 
-		  itemsCount = itemsCount -1;
+     //console.log( "wrote:", item);
+      itemsCount = itemsCount -1;
                   cleanUp();
                   //console.log("The file was saved!");
-              }); 
-           
-	console.log("--------------------------------------------");	
+              });
+
+  console.log("--------------------------------------------");
    }
 
 
-   console.log("\n *EXIT "+ itemType +"* \n");
+  console.log("\n *EXIT "+ itemType +"* \n");
 
-   function cleanUp(){
-        if (itemsCount == 0) {
-		console.log("Start CLEANUP");
+  function cleanUp(){
+    if (itemsCount == 0) {
+      console.log("Start CLEANUP");
 
+      var command = '';
+      for (var i = 0; i < numberOfContentTypes; i++) {
+        command += 'curl -o ' + contentTypes[i] + '.json ' + apiUrl + '/v1/' + contentTypes[i] + '; ';
+      }
 
-		exec('curl -o articles.json http://cyber.tcg.com/api/v1/articles; curl -o sections.json http://cyber.tcg.com/api/v1/sections; curl -o collections.json http://cyber.tcg.com/api/v1/collections; curl -o nav.json http://cyber.tcg.com/api/v2/collections;', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
+      console.log(command);
+
+      exec(command + 'curl -o nav.json ' + apiUrl + '/v2/collections;', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        exec('mongodump --db test-keystone', (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log("done.");
+        });
+
+      });
+
+    }
   }
-  console.log(`stdout: ${stdout}`);
-  //console.log(`stderr: ${stderr}`);
-
-   exec('mongodump --db test-keystone', (error, stdout, stderr) => {
-
-      if (error) {
-        console.error(`exec error: ${error}`);
-      return;
-      }    
-
-      console.log("done.");
-     });
- 
-});
-
-	}
-   }
 }
 
 // mongoexport --db test-keystone --collection collections --out collections.json --jsonArray
 // mongoexport --db test-keystone --collection sections --out sections.json --jsonArray
 // mongoexport --db test-keystone --collection articles --out articles.json --jsonArray
 
-// upgrade to mongo 3 so that we can use the following: 
+// upgrade to mongo 3 so that we can use the following:
 // mongodump --db test-keystone --gzip --archive=test-keystone
 // mongorestore --gzip --archive=test-keystone
